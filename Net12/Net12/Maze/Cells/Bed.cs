1945660269029886895e0f39cbc4208827d9ec9c
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2021-10-21 21:04:50 +0300

    CurrentFatigue

---
+++ Net12/Net12/Maze/Cells/Bed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net12.Maze.Cells
+{
+    class Bed: BaseCell
+    {
+        public override bool TryToStep()
+        {
+            Maze.Hero.CurrentFatigue;
+            return true;
+        }
+
+    }
+}
