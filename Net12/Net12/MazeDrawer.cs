c0f8ff6d420af69eaf43311e5a844155e9603329
RentExpress <RentExpress@DESKTOP-EDSCJP7>
2021-10-21 20:11:35 +0300

    teat

--- Net12/Net12/MazeDrawer.cs
+++ Net12/Net12/MazeDrawer.cs
@@ -17,7 +17,7 @@ namespace Net12
                 for (int x = 0; x < maze.Width; x++)
                 {
                     var cell = maze[x, y];
-                    if (maze.Hero.X == x && maze.Hero.Y == y)
+                    if (maze.Hero.X == x && maze.Hero.Y == y)//
                     {
                         Console.Write("@");
                     }
