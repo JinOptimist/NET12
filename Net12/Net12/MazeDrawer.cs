12a01602aceb2cbf0db479d2acfe68d7c2f9832e
JinOptimist <88jin@mail.ru>
2021-10-25 19:36:13 +0300

    Template for enemy

--- Net12/Net12/MazeDrawer.cs
+++ Net12/Net12/MazeDrawer.cs
@@ -17,7 +17,7 @@ namespace Net12
             {
                 for (int x = 0; x < maze.Width; x++)
                 {
-                    var cell = maze[x, y];
+                    var cell = maze.GetCellOrUnit(x, y);
                     if (maze.Hero.X == x && maze.Hero.Y == y)
                     {
                         Console.Write("@");
