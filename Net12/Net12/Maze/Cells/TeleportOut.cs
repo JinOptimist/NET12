1df6b702280592d7ca1053d6988c1b0add66cfa8
Metalzonemax <metalzonemax@gmail.com>
2021-10-25 15:34:50 +0300

    001

---
+++ Net12/Net12/Maze/Cells/TeleportOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net12.Maze
+{
+    class TeleportIn : BaseCell
+    {
+        public TeleportIn(int x, int y, MazeLevel maze, int numbers) : base(x, y, maze)
+        {
+            Numbers = numbers;
+        }
+
+        public int Numbers { get; set; }
+
+        public override bool TryToStep()
+        {
+           
+            return true;
+        }
+    }
+}
