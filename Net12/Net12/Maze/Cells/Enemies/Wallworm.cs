2a3f1e1845bea36cfe1127babd49f273e05a9ee7
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2021-10-29 15:11:23 +0300

    oi bsfhgoudgjbdfjmbhsdvkjbshdfl;gnbhs;

--- Net12/Net12/Maze/Cells/Enemies/Wallworm.cs
+++ Net12/Net12/Maze/Cells/Enemies/Wallworm.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Net12.Maze.Cells.Enemies
 {
-    class Wallworm
+    public class Wallworm : BaseEnemy
     {
+        public Wallworm(int x, int y, MazeLevel maze) : base(x, y, maze)
+        {
+        }
+        public int CounterStep { get; set; } = 0;
+        public int StepsBeforeEating { get; set; } = 3;
+
+        public override void Step()
+        {
+            CounterStep++;
+            if (CounterStep > StepsBeforeEating)
+            {
+                Random random = new Random();
+                BaseCell GetRandom(List<BaseCell> cells)
+                {
+                    var index = random.Next(cells.Count);
+                    return cells[index];
+                }
+
+                var grounds = Maze.Cells.Where(x => x is Wall).ToList();
+                var randomGrounds = GetRandom(grounds);
+                Maze[randomGrounds.X, randomGrounds.Y] = new WeakWall(randomGrounds.X, randomGrounds.Y, Maze);
+
+                CounterStep = 0;
+            }
+
+        }
+
+        public override bool TryToStep()
+        {
+            return true;
+        }
     }
 }
