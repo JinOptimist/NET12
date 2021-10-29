be4bc807fb3364c797bed4e8f310a22675682eab
JuliaGorbach21 <ulia22227@yandex.ru>
2021-10-29 14:09:18 +0300

    Task 55 (UT WolfPit)

---
+++ Net12/Net12.Test/Maze/Cells/WolfPitTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net12.Test.Maze.Cells
+{
+    
+    public class WolfPitTest
+    {
+        [Test]
+        //Preparing
+        
+
+        //Act
+      
+
+        //Assert
+    }
+}
