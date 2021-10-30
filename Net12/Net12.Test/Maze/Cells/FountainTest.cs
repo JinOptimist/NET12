94558c2d6759302e276eaf2a87622b096d89e346
Metalzonemax <metalzonemax@gmail.com>
2021-10-30 14:58:49 +0300

    003

--- Net12/Net12.Test/Maze/Cells/FountainTest.cs
+++ Net12/Net12.Test/Maze/Cells/FountainTest.cs
@@ -11,30 +11,20 @@ namespace Net12.Test.Maze.Cells
     public class FountainTest
     {
         [Test]
-        [TestCase(9, 8)]
-        [TestCase(100, 99)]
-        [TestCase(0, 0)]
-        public void TryToStepTest(int hpInit, int hpResult)
+        public void TryToStepTest()
         {
-            //Preparing
+            //Arrange
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<IHero>();
-
-            heroMock.SetupProperty(x => x.Hp);
-            heroMock.Object.Hp = hpInit;
-
-            mazeMock
-                .Setup(x => x.Hero)
-                .Returns(heroMock.Object);
-            var trap = new Trap(0,0, mazeMock.Object);
-
+            mazeMock.Setup(x => x.Hero).Returns(heroMock.Object);
+            var fount = new Fountain(0, 0, mazeMock.Object);
             //Act
-            var answer = trap.TryToStep();
+            var answer = fount.TryToStep();
 
             //Assert
-            Assert.AreEqual(true, answer, "We must have possibility to step on the Fountain");
-            Assert.AreEqual(hpResult, heroMock.Object.Hp);
-            mazeMock.Verify(x => x.ReplaceCell(It.IsAny<BaseCell>()), Times.AtLeastOnce);
+            Assert.AreEqual(true, answer);
+           
+
         }
     }
 }
