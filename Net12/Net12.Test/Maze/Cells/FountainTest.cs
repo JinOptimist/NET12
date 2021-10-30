1f9c86ccec4a52b0e490f27d6bbee79c66cbaaed
Metalzonemax <metalzonemax@gmail.com>
2021-10-30 15:37:37 +0300

    003

--- Net12/Net12.Test/Maze/Cells/FountainTest.cs
+++ Net12/Net12.Test/Maze/Cells/FountainTest.cs
@@ -11,19 +11,24 @@ namespace Net12.Test.Maze.Cells
     public class FountainTest
     {
         [Test]
-        public void TryToStepTest()
+        [TestCase (21, 1)]
+        [TestCase(19, 0)]
+        public void TryToStepTest(int fatiqueInit, int fatiqueResult)
         {
             //Arrange
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<IHero>();
             mazeMock.Setup(x => x.Hero).Returns(heroMock.Object);
             var fount = new Fountain(0, 0, mazeMock.Object);
+            heroMock.SetupProperty(x => x.CurrentFatigue);
+            heroMock.Object.CurrentFatigue = fatiqueInit;
             //Act
             var answer = fount.TryToStep();
 
             //Assert
             Assert.AreEqual(true, answer);
-           
+            Assert.AreEqual(fatiqueResult, heroMock.Object.CurrentFatigue);
+
 
         }
     }
