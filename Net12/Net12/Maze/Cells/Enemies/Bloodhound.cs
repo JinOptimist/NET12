69e5dd2cef058785e113f4e46ceb919f04c3a734
Metalzonemax <metalzonemax@gmail.com>
2021-10-30 20:30:33 +0300

    001

---
+++ Net12/Net12/Maze/Cells/Enemies/Bloodhound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net12.Maze.Cells.Enemies
+{
+    class Bloodhound : BaseEnemy
+    {
+        public Bloodhound(int x, int y, MazeLevel maze) : base(x, y, maze)
+        {
+        }
+
+        public override void Step()
+        {
+          
+        }
+
+        public override bool TryToStep()
+        {
+            return true;
+        }
+    }
+}
