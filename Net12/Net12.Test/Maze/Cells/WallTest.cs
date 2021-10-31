f05d981a3ca185cb4c94f0ed3b70573292b750d6
andreyuqaaaa <55231367+andreqyuqaaaa@users.noreply.github.com>
2021-10-31 14:21:16 +0300

    Create test wall

--- Net12/Net12.Test/Maze/Cells/WallTest.cs
+++ Net12/Net12.Test/Maze/Cells/WallTest.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using Net12.Maze;
+using Net12.Maze.Cells;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Net12.Test.Maze.Cells
 {
+    
     class WallTest
     {
+        [Test]
+        public void TryToStep()
+        {
+            //Preparing
+            var mazeMock = new Mock<IMazeLevel>();
+            var heroMock = new Mock<IHero>();
+
+            mazeMock
+                .Setup(x => x.Hero)
+                .Returns(heroMock.Object);
+            var wall = new Wall(0, 0, mazeMock.Object);
+
+
+            //Act
+            var answer = wall.TryToStep();
+
+
+            //Assert
+            Assert.AreEqual(false, answer, "We can't to step on the wall");
+        }
     }
 }
