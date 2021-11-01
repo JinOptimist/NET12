82bb796e69b8562159e287268218bfa832fad034
Николай Толкачев <darkmed@mail.ru>
2021-11-01 21:07:40 +0300

    AddTask63

--- Net12/Net12.Test/Maze/Cells/VitalityPotionTest.cs
+++ Net12/Net12.Test/Maze/Cells/VitalityPotionTest.cs
@@ -11,31 +11,26 @@ namespace Net12.Test.Maze.Cells
     public class VitalityPotionTest
     {
         [Test]
-        [TestCase(30,35)]
-        [TestCase(100,105)]
-        [TestCase(10,15)]
-        public void TryToStepTest(int maxFatigueinit, int maxFatigueResult)
+        [TestCase(30, 35, 5)]
+        [TestCase(100, 107, 7)]
+        [TestCase(10, 20, 10)]
+        public void TryToStepTest(int maxFatigueinit, int maxFatigueResult, int vitalityPotionEffect)
         {
-            
+
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<IHero>();
             heroMock.SetupProperty(x => x.MaxFatigue);
             heroMock.Object.MaxFatigue = maxFatigueinit;
 
-            mazeMock.Setup(x => x.Hero).Returns(heroMock.Object); 
+            mazeMock.Setup(x => x.Hero).Returns(heroMock.Object);
 
-
-            
-            var vitalityPotion = new VitalityPotion(0, 0, mazeMock.Object,0);
+            var vitalityPotion = new VitalityPotion(0, 0, mazeMock.Object, vitalityPotionEffect);
 
 
             var answer = vitalityPotion.TryToStep();
 
-
-
-
-            Assert.AreEqual(true,answer, "We must have possibility to step on the trap");
-            Assert.AreEqual(maxFatigueResult, heroMock.Object.MaxFatigue +5);
+            Assert.AreEqual(true, answer, "We must have possibility to step on the trap");
+            Assert.AreEqual(maxFatigueResult, heroMock.Object.MaxFatigue);
             mazeMock.Verify(x => x.ReplaceCell(It.IsAny<BaseCell>()), Times.AtLeastOnce);
         }
 
