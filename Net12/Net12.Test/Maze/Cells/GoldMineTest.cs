836d2b2f8350a997e5c5467626d3eba784f81e39
yuulianaa <starrry.sky@yandex.ru>
2021-11-01 17:52:14 +0300

    test

---
+++ Net12/Net12.Test/Maze/Cells/GoldMineTest.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using Net12.Maze;
+using Net12.Maze.Cells;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net12.Test.Maze.Cells
+{
+    public class GoldMineTest
+    {
+        [Test]
+        [TestCase(9, 12)]
+        [TestCase(100, 103)]
+        public void TryToStepTest(int moneyInit, int moneyResult)
+        {
+            //Preparing
+            var mazeMock = new Mock<IMazeLevel>();
+            var heroMock = new Mock<IHero>();
+
+            heroMock.SetupProperty(x => x.Money);
+            heroMock.Object.Money = moneyInit;
+
+            mazeMock
+                .Setup(x => x.Hero)
+                .Returns(heroMock.Object);
+            var goldMine = new GoldMine(0, 0, mazeMock.Object);
+
+            //Act
+            var answer = true;
+            for (var i = goldMine.currentGoldMineMp; i < goldMine.goldMineMaxHp; i++)
+            {
+                answer = goldMine.TryToStep();
+            }
+
+            //Assert
+            Assert.AreEqual(false, answer, "We mustn't have possibility to step on the goldmine");
+
+            Assert.AreEqual(moneyResult, heroMock.Object.Money);
+            mazeMock.Verify(x => x.ReplaceCell(It.IsAny<BaseCell>()), Times.AtLeastOnce);
+        }
+    }
+    
+}
