df42886941927feb58589a06689bc0a10ffb635d
JinOptimist <88jin@mail.ru>
2021-11-01 20:39:41 +0300

    Formula Lvov

--- Net12/CoolFormulaString/Formula.cs
+++ Net12/CoolFormulaString/Formula.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CoolFormulaString
 {
@@ -6,7 +9,41 @@ namespace CoolFormulaString
     {
         public string Calc(string formula)
         {
-            return "42";
+            var regex = new Regex(@"\d{1,}[+-]\d{1,}");
+            var match = regex.Match(formula);
+            if (match.Success)
+            {
+                var answer = CalcBasicPhares(match.Value);
+                formula = formula.Replace(match.Value, answer);
+            }
+
+            return formula;
+        }
+
+        private string CalcBasicPhares(string formula)
+        {
+            var operations = new char[] { '+', '*', '-', '/' };
+            var operationIndex = formula.IndexOfAny(operations);
+            var operationSymbol = formula[operationIndex];
+
+            var numbers = formula
+                .Split(operations)
+                .Select(str => int.Parse(str))
+                .ToList();
+
+            switch (operationSymbol)
+            {
+                case '+':
+                    return (numbers[0] + numbers[1]).ToString();
+                case '-':
+                    return (numbers[0] - numbers[1]).ToString();
+                case '*':
+                    return (numbers[0] * numbers[1]).ToString();
+                case '/':
+                    return (numbers[0] / numbers[1]).ToString();
+            }
+
+            return "Error";
         }
     }
 }
