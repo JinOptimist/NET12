a817f35646f65ba15ffb45bbbd2b66fa2025e185
andreyuqaaaa <55231367+andreqyuqaaaa@users.noreply.github.com>
2021-11-04 16:09:59 +0300

    Tavent test finish

--- Net12/Net12.Test/Maze/Cells/TavernTest.cs
+++ Net12/Net12.Test/Maze/Cells/TavernTest.cs
@@ -11,29 +11,34 @@ namespace Net12.Test.Maze.Cells
     class TavernTest
     {
         [Test]
-        [TestCase(0, 0)]
-        [TestCase(1, 0)]
-        [TestCase(2, 1)]
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(1, 0, 0, 0)]
+        [TestCase(2, 1, 4, 0)]
+        [TestCase(2, 1, 50, 45)]
 
 
-        public void TryToStep(int moneyInit, int moneyResult)
+        public void TryToStep(int moneyInit, int moneyResult, int CurrentFatiqueInit , int CurrentFatiqueResult)
         {
             //Preparing
             var mazeMock = new Mock<IMazeLevel>();
             var heroMock = new Mock<IHero>();
+
             mazeMock
                 .Setup(x => x.Hero)
                 .Returns(heroMock.Object);
+
             var tavern = new Tavern(0, 0, mazeMock.Object);
 
             heroMock.SetupProperty(x => x.Money);
             heroMock.Object.Money = moneyInit;
 
+            heroMock.SetupProperty(x => x.CurrentFatigue);
+            heroMock.Object.CurrentFatigue = CurrentFatiqueInit;
 
 
             //Act
             var ansver = tavern.TryToStep();
-
+            var ansverFatique = CurrentFatiqueResult;
 
             //Accert
             if (moneyInit <= 0)
@@ -43,8 +48,17 @@ namespace Net12.Test.Maze.Cells
             }
             else
             {
+                if(CurrentFatiqueInit <= 5)
+                {
+                    CurrentFatiqueInit = 0;
+                }
+                else
+                {
+                    CurrentFatiqueInit -= 5;
+                }
                 Assert.AreEqual(true, ansver);
                 Assert.AreEqual(moneyResult, heroMock.Object.Money);
+                Assert.AreEqual(CurrentFatiqueResult, ansverFatique);
             }
 
 
