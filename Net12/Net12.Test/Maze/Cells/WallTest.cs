d71adefd1ca59f48c1a6ec37b459cf92b977d1f6
andreyuqaaaa <55231367+andreqyuqaaaa@users.noreply.github.com>
2021-11-04 16:48:39 +0300

    test wall finish

--- Net12/Net12.Test/Maze/Cells/WallTest.cs
+++ Net12/Net12.Test/Maze/Cells/WallTest.cs
@@ -16,11 +16,9 @@ namespace Net12.Test.Maze.Cells
         {
             //Preparing
             var mazeMock = new Mock<IMazeLevel>();
-            var heroMock = new Mock<IHero>();
 
-            mazeMock
-                .Setup(x => x.Hero)
-                .Returns(heroMock.Object);
+
+
             var wall = new Wall(0, 0, mazeMock.Object);
 
 
