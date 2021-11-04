a1835841e9fa98e9788d77a201610011d506bcf7
JinOptimist <88jin@mail.ru>
2021-11-04 19:02:48 +0300

    Update test for Tavern

--- Net12/Net12.Test/Maze/Cells/TavernTest.cs
+++ Net12/Net12.Test/Maze/Cells/TavernTest.cs
@@ -10,58 +10,59 @@ namespace Net12.Test.Maze.Cells
 {
     class TavernTest
     {
-        [Test]
-        [TestCase(0, 0, 0, 0)]
+        private Mock<IMazeLevel> _mazeMock;
+        private Mock<IHero> _heroMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mazeMock = new Mock<IMazeLevel>();
+            _heroMock = new Mock<IHero>();
+
+            _mazeMock
+                .Setup(x => x.Hero)
+                .Returns(_heroMock.Object);
+
+            _heroMock.SetupProperty(x => x.Money);
+            _heroMock.SetupProperty(x => x.CurrentFatigue);
+        }
+
         [TestCase(1, 0, 0, 0)]
         [TestCase(2, 1, 4, 0)]
         [TestCase(2, 1, 50, 45)]
-
-
-        public void TryToStep(int moneyInit, int moneyResult, int CurrentFatiqueInit , int CurrentFatiqueResult)
+        public void TryToStep_RichHero(int moneyInit, int moneyResult, int currentFatiqueInit , int currentFatiqueResult)
         {
             //Preparing
-            var mazeMock = new Mock<IMazeLevel>();
-            var heroMock = new Mock<IHero>();
-
-            mazeMock
-                .Setup(x => x.Hero)
-                .Returns(heroMock.Object);
-
-            var tavern = new Tavern(0, 0, mazeMock.Object);
-
-            heroMock.SetupProperty(x => x.Money);
-            heroMock.Object.Money = moneyInit;
-
-            heroMock.SetupProperty(x => x.CurrentFatigue);
-            heroMock.Object.CurrentFatigue = CurrentFatiqueInit;
-
+            var tavern = new Tavern(0, 0, _mazeMock.Object);
+            _heroMock.Object.Money = moneyInit;
+            _heroMock.Object.CurrentFatigue = currentFatiqueInit;
 
             //Act
             var ansver = tavern.TryToStep();
-            var ansverFatique = CurrentFatiqueResult;
 
             //Accert
-            if (moneyInit <= 0)
-            {
-                Assert.AreEqual(false, ansver, "if Hero doesn't have money, he can't to step on the Tavern");
-                Assert.AreEqual(moneyResult, heroMock.Object.Money);
-            }
-            else
-            {
-                if(CurrentFatiqueInit <= 5)
-                {
-                    CurrentFatiqueInit = 0;
-                }
-                else
-                {
-                    CurrentFatiqueInit -= 5;
-                }
-                Assert.AreEqual(true, ansver);
-                Assert.AreEqual(moneyResult, heroMock.Object.Money);
-                Assert.AreEqual(CurrentFatiqueResult, ansverFatique);
-            }
+            Assert.AreEqual(true, ansver);
+            Assert.AreEqual(moneyResult, _heroMock.Object.Money);
+            Assert.AreEqual(currentFatiqueResult, _heroMock.Object.CurrentFatigue);
+        }
 
+        [TestCase(0, 0)]
+        [TestCase(0, 10)]
+        [TestCase(0, 13)]
+        public void TryToStep_NoMoneyNoHonie(int currentFatiqueInit)
+        {
+            //Preparing
+            _heroMock.Object.Money = 0;
+            _heroMock.Object.CurrentFatigue = currentFatiqueInit;
 
+            var tavern = new Tavern(0, 0, _mazeMock.Object);
+
+            //Act
+            var answer = tavern.TryToStep();
+
+            //Accert
+            Assert.AreEqual(false, answer, "if Hero doesn't have money, he can't to step on the Tavern");
+            Assert.AreEqual(currentFatiqueInit, _heroMock.Object.CurrentFatigue);
         }
     }
 }
