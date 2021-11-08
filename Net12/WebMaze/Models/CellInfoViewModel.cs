cda15c6f9bc597f4fb8191d4fa595c58c175e866
Archie-Red <archibubkin@gmail.com>
2021-11-08 20:48:17 +0300

    View

---
+++ Net12/WebMaze/Models/CellInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.Models
+{
+    public class CellInfoViewModel
+    {
+        public string ImageUrl { get; set; }
+        public string Desc { get; set; }
+        public bool CanStep { get; set; }
+        public string Multiplicity { get; set; }
+        public int HP { get; set; }
+        public int Money { get; set; }
+
+
+    }
+}
