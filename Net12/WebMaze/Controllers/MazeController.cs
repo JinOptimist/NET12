a5655d5916bbd7fa3d497582368b503bc34d3c27
Влад <uv.ev1971@mail.ru>
2021-11-08 20:47:33 +0300

    fix

--- Net12/WebMaze/Controllers/MazeController.cs
+++ Net12/WebMaze/Controllers/MazeController.cs
@@ -22,11 +22,21 @@ namespace WebMaze.Controllers
         public IActionResult Tavern()
         {
             var model = new CellInfoViewModel();
-   
             model.CanStep = true;
-            model.ImageUrl = "img/tavern.jpg";
+            model.ImageUrl = "/images/tavern.jpg";
             model.Desc = "Tavern, where you can spend your time.";
             model.Spec = "HP: Building";
+
+            return View(model);
+        }
+
+        public IActionResult Walker()
+        {
+            var model = new CellInfoViewModel();
+            model.CanStep = true;
+            model.ImageUrl = "/images/walkerico.jpg";
+            model.Desc = "A walking enemy of the hero who, when meeting the hero, deals damage to him.";
+            model.Spec = "HP: Divine protection";
 
             return View(model);
         }
