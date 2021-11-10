7cb8a7bb274883da8a55272019f26bbc1442361e
Kadabras <samex45@gmail.com>
2021-11-10 09:36:49 +0300

    omega

--- Net12/WebMaze/Controllers/MazeController.cs
+++ Net12/WebMaze/Controllers/MazeController.cs
@@ -24,12 +24,24 @@ namespace WebMaze.Controllers
         {
             var model = new CellInfoViewModel();
 
-            model.CanStep = false;
+            model.CanStep = true;
             model.ImageUrl = "/images/VitalityPotion.webp";
             model.Desc = "Увеличивает максимальное количество усталости на 5";
 
 
             return View(model);
         }
+        public IActionResult Slime()
+        {
+            var model = new CellInfoViewModel();
+
+            model.CanStep = false;
+            model.ImageUrl = "/images/Slime.png";
+            model.Desc = "Ходит по лабиринту и с некоторой вероятностью создает Монетку. При отсуствии пути перепрыгивает на случайную землю.";
+
+
+            return View(model);
+        }
+
     }
 }
