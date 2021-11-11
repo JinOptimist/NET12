84fefe994b69912be71e30b431832adbfc9b3206
JuliaGorbach21 <ulia22227@yandex.ru>
2021-11-11 18:51:17 +0300

    Done

--- Net12/WebMaze/Controllers/MazeController.cs
+++ Net12/WebMaze/Controllers/MazeController.cs
@@ -19,5 +19,27 @@ namespace WebMaze.Controllers
 
             return View(model);
         }
+
+        public IActionResult Geyser()
+        {
+            var model = new CellInfoViewModel();
+
+            model.CanStep = false;
+            model.ImageUrl = "/images/Geyser.jpg";
+            model.Desc = "When the hero steps on a geyser , if there are earth - type cells around it , they change to the puddle type";
+
+            return View(model);
+        }
+
+        public IActionResult Puddle()
+        {
+            var model = new CellInfoViewModel();
+
+            model.CanStep = true;
+            model.ImageUrl = "/images/Puddle.jpg";
+            model.Desc = "There is the message 'wap - wap' on the screen when the hero steps on a puddle.";
+
+            return View(model);
+        }
     }
 }
