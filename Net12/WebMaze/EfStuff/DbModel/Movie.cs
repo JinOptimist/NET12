f8b56c6363f9e98d6b608b5ceafd26093eee9920
s@gmail.com <s@gmail.com>
2021-11-11 21:57:35 +0300

    start

---
+++ Net12/WebMaze/EfStuff/DbModel/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.EfStuff.DbModel
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string TitleGame { get; set; }
+        public string TitleMovie { get; set; }
+        public int release { get; set; }
+        public string Link { get; set; }
+
+    }
+}
