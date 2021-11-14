0c303d2d5d85dccfb1b147f85394552ea4a3e040
Metalzonemax <metalzonemax@gmail.com>
2021-11-14 15:45:15 +0300

    001

---
+++ Net12/WebMaze/Models/NewCellSuggestionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.Models
+{
+    public class NewCellSuggestionsViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int MoneyChange { get; set; }
+        public int HealtsChange { get; set; }
+        public int FatigueChange { get; set; }
+        public string UserName { get; set; }
+    }
+}
