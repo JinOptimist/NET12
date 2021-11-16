4fbb40f12348b707928081d686aeb6b6311449d9
Влад <uv.ev1971@mail.ru>
2021-11-16 12:38:27 +0300

    fix bugs in main

--- Net12/WebMaze/Controllers/MazeController.cs
+++ Net12/WebMaze/Controllers/MazeController.cs
@@ -18,15 +18,12 @@ namespace WebMaze.Controllers
             return View(maze);
         }
 
-            return View(model);
-        }
-
         public IActionResult Goldmine()
         {
             var model = new CellInfoViewModel();
 
             model.CanStep = false;
-            model.ImageUrl = "/images/goldmine.jpg";
+            model.Url = "/images/goldmine.jpg";
             model.Desc = "Goldmine has 3 hp and gives you 1 coin every hit.";
 
             return View(model);
