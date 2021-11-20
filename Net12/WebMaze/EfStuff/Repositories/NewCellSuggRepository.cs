d916e58d32b098f045c505bfbec5ecd78307f8bb
Metalzonemax <metalzonemax@gmail.com>
2021-11-20 16:37:42 +0300

    final

--- Net12/WebMaze/EfStuff/Repositories/NewCellSuggRepository.cs
+++ Net12/WebMaze/EfStuff/Repositories/NewCellSuggRepository.cs
@@ -6,56 +6,12 @@ using WebMaze.EfStuff.DbModel;
 
 namespace WebMaze.EfStuff.Repositories
 {
-    public class NewCellSuggRepository
+    public class NewCellSuggRepository : BaseRepository<NewCellSuggestion>
     {
-        private WebContext _webContext;
-
-        public NewCellSuggRepository(WebContext webContext)
+        public NewCellSuggRepository(WebContext webContext) : base(webContext)
         {
-            _webContext = webContext;
-        }
 
-        public NewCellSuggestion Get(long id)
-        {
-            return _webContext.NewCellSuggestions.SingleOrDefault(x => x.Id == id);
-        }
-
-        public List<NewCellSuggestion> GetAll()
-        {
-            return _webContext
-            .NewCellSuggestions
-            .ToList();
-        }
-
-        public NewCellSuggestion GetRandomUser()
-        {
-            return _webContext.NewCellSuggestions.First();
-        }
-
-        public void Save(NewCellSuggestion user)
-        {
-            if (user.Id > 0)
-            {
-                _webContext.Update(user);
-            }
-            else
-            {
-                _webContext.NewCellSuggestions.Add(user);
-            }
-
-            _webContext.SaveChanges();
-        }
-
-        public void Remove(NewCellSuggestion user)
-        {
-           
-            Save(user);
-        }
-
-        public void Remove(long userId)
-        {
-            var user = Get(userId);
-            Remove(user);
         }
     }
+
 }
