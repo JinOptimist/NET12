bdaf90a041594b680c7f6ee7f933db1ba14a12ff
Kadabras <samex45@gmail.com>
2021-11-21 18:03:06 +0300

    Revert "v3"
    This reverts commit 78aee9ba9746b036c3bfdb41ada1d106b6028e08.

---
+++ Net12/WebMaze/Migrations/20211111180842_Difficult.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMaze.Migrations
+{
+    public partial class Difficult : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MazeDifficultProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HeroMoney = table.Column<int>(type: "int", nullable: false),
+                    HeroMaxHp = table.Column<int>(type: "int", nullable: false),
+                    HeroMaxFatigue = table.Column<int>(type: "int", nullable: false),
+                    CoinCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MazeDifficultProfiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MazeDifficultProfiles");
+        }
+    }
+}
---
+++ Net12/WebMaze/Migrations/20211117085734_aaa.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMaze.Migrations
+{
+    public partial class aaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CreaterId",
+                table: "MazeDifficultProfiles",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MazeDifficultProfiles_CreaterId",
+                table: "MazeDifficultProfiles",
+                column: "CreaterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MazeDifficultProfiles_Users_CreaterId",
+                table: "MazeDifficultProfiles",
+                column: "CreaterId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MazeDifficultProfiles_Users_CreaterId",
+                table: "MazeDifficultProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MazeDifficultProfiles_CreaterId",
+                table: "MazeDifficultProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "CreaterId",
+                table: "MazeDifficultProfiles");
+        }
+    }
+}
