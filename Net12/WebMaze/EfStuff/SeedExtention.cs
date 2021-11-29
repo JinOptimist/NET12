d584d6b20bd8792944d70666a27fc43b2f098671
JinOptimist <88jin@mail.ru>
2021-11-29 21:51:03 +0300

    Add Seed method

---
+++ Net12/WebMaze/EfStuff/SeedExtention.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMaze.EfStuff.DbModel;
+using WebMaze.EfStuff.Repositories;
+
+namespace WebMaze.EfStuff
+{
+    public static class SeedExtention
+    {
+        public const string DefaultAdminName = "admin";
+        public static IHost Seed(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                SeedUser(scope);
+            }
+
+            return host;
+        }
+
+        private static void SeedUser(IServiceScope scope)
+        {
+            var userRepository = scope.ServiceProvider.GetService<UserRepository>();
+            var admin = userRepository.GetUserByName(DefaultAdminName);
+            if (admin == null)
+            {
+                admin = new User()
+                {
+                    Name = DefaultAdminName,
+                    Password = "admin",
+                    Coins = 100,
+                    IsActive = true
+                };
+
+                userRepository.Save(admin);
+            }
+        }
+    }
+}
