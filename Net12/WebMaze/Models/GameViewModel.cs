53ee47351d19024e7de44bd0fe3e376399ac0f60
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2021-11-29 21:02:10 +0300

    /

--- Net12/WebMaze/Models/GameViewModel.cs
+++ Net12/WebMaze/Models/GameViewModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebMaze.Models
 {
     public class GameViewModel
     {
+        [MinLength(2, ErrorMessage = "Name of game is too short")]
         public string Name { get; set; }
+
+        [MinLength(2, ErrorMessage = "Name of game is too short")]
         public string Genre { get; set; }
         public int YearOfProd { get; set; }
         public string Desc { get; set; }
