5f397201552600ba625f081f90fb2e719beb3516
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2021-12-02 19:24:51 +0300

    half done

--- Net12/WebMaze/Models/ValidationAttributes/CheckYearAttribute.cs
+++ Net12/WebMaze/Models/ValidationAttributes/CheckYearAttribute.cs
@@ -8,5 +8,30 @@ namespace WebMaze.Models.ValidationAttributes
 {
     public class CheckYearAttribute : ValidationAttribute
     {
+        public override bool IsValid(object value)
+        {         
+            if (value != null && !(value is int))
+            {
+                return false;
+                //throw new ArgumentException("CheckYear Attribute can work only with int");
+            }
+
+            var number = (int?)value;
+
+            if (!(number >= 0 && number <= 99 || number >= 1000 && number <= 9999))
+            {
+                return false;
+                //throw new ArgumentException("CheckYear Attribute can work only with 'yyyy' or 'yy' format");
+            }
+
+            return true;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.IsNullOrEmpty(ErrorMessage)
+                ? "Impossible to read year. Please use form as 'yyyy' or 'yy'"
+                : ErrorMessage;
+        }
     }
 }
