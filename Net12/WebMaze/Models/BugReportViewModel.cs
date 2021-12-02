21177350c6ee255672f775547aa448640cf29ad4
yuulianaa <starrry.sky@yandex.ru>
2021-12-02 19:24:42 +0300

    almost done

--- Net12/WebMaze/Models/BugReportViewModel.cs
+++ Net12/WebMaze/Models/BugReportViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMaze.Models.ValidationAttributes;
 
 namespace WebMaze.Models
 {
@@ -10,6 +11,8 @@ namespace WebMaze.Models
     {
         public string CreaterName { get; set; }
 
+        [Required(ErrorMessage = "fff")]
+        [BadWordsBugReports("damn", "hate", "ass")]
         [MinLength(10, ErrorMessage = "This description is too short, write more please :)")]
         public string Description { get; set; }
     }
