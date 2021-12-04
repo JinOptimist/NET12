c8eeced82117aa42c93048f4465d20f0a52c5398
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2021-12-04 19:13:26 +0300

    002

--- Net12/WebMaze/Models/ValidationAttributes/SwearWordAttribute.cs
+++ Net12/WebMaze/Models/ValidationAttributes/SwearWordAttribute.cs
@@ -9,16 +9,24 @@ namespace WebMaze.Models.ValidationAttributes
     public class SwearWordAttribute : ValidationAttribute
     {
         private string[] _swearWord;
-        private string[] _swearWordBase = { "Asshole", "Bitch", "Dick", "Faggot", "Fuck", "Nigger", "Shit" };
+        private string[] _swearWordBase = { "Ass", "Asshole", "Bitch", "Dick", "Faggot", "Fuck", "Nigger", "Shit" };
+        private string[] _swearWordAll;
         public SwearWordAttribute(params string[] swearWord)
         {
             _swearWord = swearWord;
+            _swearWordAll = Foo(_swearWord, _swearWordBase);
+        }
+
+        private string[] Foo(string[] arr1, string[] arr2)
+        {
+            var arr3 = arr1.Concat(arr2).ToArray();
+            return arr3;
         }
 
         public override string FormatErrorMessage(string name)
         {
             return string.IsNullOrEmpty(ErrorMessage)
-                ? $"We don't like you. Cause you use bad word like: \"{string.Join(" ", _swearWordBase)}\""
+                ? $"We don't like you. Cause you use bad word like: \"{string.Join(", ", _swearWordAll)}\""
                 : ErrorMessage;
         }
 
@@ -36,9 +44,8 @@ namespace WebMaze.Models.ValidationAttributes
 
             var line = value as string;
 
-            return _swearWordBase.All(swearWord => !line.Contains(swearWord));
+            return _swearWordAll.All(swearWord => !line.ToLower().Contains(swearWord.ToLower()));
 
-            //&& _swearWordBase.All(swearWord => !line.ToLower().Contains(swearWord));
         }
     }
 }
