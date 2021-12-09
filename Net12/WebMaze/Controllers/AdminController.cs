5c3f298224a8a24639f298e02c9662be7a29df2c
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2021-12-09 17:24:27 +0300

    004

---
+++ Net12/WebMaze/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebMaze.EfStuff.Repositories;
+using WebMaze.Models;
+using WebMaze.Services;
+
+namespace WebMaze.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private IMapper _mapper;
+
+        public AdminController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        public IActionResult ViewPermission()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
