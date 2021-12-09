9c5f328034bb5d60f0f6a6fd9d2becdcd275b5d6
Aleksei <TheEclipsE@tut.by>
2021-12-09 21:43:17 +0300

    fixed

--- Net12/WebMaze/Controllers/MazeController.cs
+++ Net12/WebMaze/Controllers/MazeController.cs
@@ -100,7 +100,7 @@ namespace WebMaze.Controllers
         {
             //TODO: CHOOSE DIFFICULITY
             //  var maze = new MazeBuilder().Build(10, 10, 100, 100, true);
-            var maze = new MazeBuilder().Build(10, 10, 100, 100, false);
+            var maze = new MazeBuilder().Build(10, 10, 100, 100,GetCoinsFromMaze, false);
             var model = _mapper.Map<MazeLevelModel>(maze);
             model.IsActive = true;
 
