8d5dcbd6229cfa1853845078c08c5574f0c9a73f
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2021-12-10 16:17:51 +0300

    it works

--- Net12/WebMaze/Controllers/AdminController.cs
+++ Net12/WebMaze/Controllers/AdminController.cs
@@ -15,18 +15,22 @@ namespace WebMaze.Controllers
 {
     public class AdminController : Controller
     {
-
+        private PermissionRepository _permissionRepository;
         private IMapper _mapper;
 
-        public AdminController(IMapper mapper)
+        public AdminController(PermissionRepository permissionRepository, IMapper mapper)
         {
+            _permissionRepository = permissionRepository;
             _mapper = mapper;
         }
 
         [Authorize]
         public IActionResult ViewPermission()
         {
-            return View();
+            var permissionViewModels = new List<PermissionViewModel>();
+            permissionViewModels = _permissionRepository.GetAll()
+                .Select(x => _mapper.Map<PermissionViewModel>(x)).ToList();
+            return View(permissionViewModels);
         }
 
 
