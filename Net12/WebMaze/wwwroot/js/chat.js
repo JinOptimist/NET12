8d855ae8627a6877ed4af141b2d3ca3f6eeef70c
JinOptimist <88jin@mail.ru>
2021-12-20 21:57:16 +0300

    Cool chat

--- Net12/WebMaze/wwwroot/js/chat.js
+++ Net12/WebMaze/wwwroot/js/chat.js
@@ -5,24 +5,38 @@
 
     //Что делать, когда пршло новое сообщение
     hubConnection.on("NewMessage", function (message, name) {
-        addMessage(`${name}: ${message}`);
+        addMessage(name, message);
     });
 
     hubConnection.on("Enter", function (name) {
-        addMessage(`User '${name}' enter to site`);
+        addMessage(name, `enter to site`);
     });
 
-    function addMessage(text) {
+    function addMessage(userName, text) {
         var messageBlock = $('<div>');
-        messageBlock.text(text);
         messageBlock.addClass('message');
+
+        var authorSpan = $('<span>');
+        authorSpan.addClass('user-name');
+        authorSpan.text(userName);
+        messageBlock.append(authorSpan);
+
+        var textSpan = $('<span>');
+        textSpan.addClass('message-text');
+        textSpan.text(text);
+        messageBlock.append(textSpan);
+        
         $('.messages').append(messageBlock);
+
+        const topPosition = $(".message:last").position().top;
+        $(".messages").animate({ scrollTop: topPosition }, 300);
     }
 
     //Отправить новое сообщзение на сервер
     $('.send-message').click(function () {
         let message = $('.new-message').val();
         hubConnection.invoke("NewMessage", message);
+        $('.new-message').val('');
     });
 
     hubConnection.start();
