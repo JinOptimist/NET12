c899b389b430bdbdd3ef3777cbaf8bbd74707ef0
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-10 21:24:35 +0300

    "TeleportInInfo add color when clicked" was made

---
+++ Net12/WebMaze/wwwroot/js/Cells/teleportInInfo.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    $(".click-to-add-color").click(function () {
+
+        if ($(".area-to-change-color").hasClass("click-to-change-color"))
+        {
+            $(".area-to-change-color").toggleClass("click-to-change-color");
+        }
+        else
+        {
+            $(".area-to-change-color").addClass("click-to-change-color");
+        }
+    });
+    //".area-to-change-color"
+    //'.area-to-change-color'
+});
+
+
+
