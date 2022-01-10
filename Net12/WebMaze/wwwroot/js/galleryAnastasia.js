c5eca503bdf097af0d3e92e8a1d173b1e5b6ccf8
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-10 21:52:31 +0300

    1st

---
+++ Net12/WebMaze/wwwroot/js/galleryAnastasia.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+
+    $('h1').click(function () {
+        const second = new Date().getSeconds();
+        $('h1 .seconds').text(second);
+
+    });
+
+
+});
+
+
