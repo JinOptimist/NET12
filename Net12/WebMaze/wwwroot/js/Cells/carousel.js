2becad7428902bdd1193c0daf801c965d3a6ef86
Kadabras <samex45@gmail.com>
2022-01-12 13:07:53 +0300

    carousel

---
+++ Net12/WebMaze/wwwroot/js/Cells/carousel.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    let steps = 0;
+    let width;
+
+    $.get('/CellInfo/GetImage')
+        .done(function (urls) {
+
+            width = $('.carousel-block span').css('width');
+
+            setImage();
+
+            $('.carousel-image').click(function () {
+
+                $('.current-image').animate({
+
+                    width: 0
+                },
+                    2000,
+                    function () {
+                    });
+
+                $('.next-image').animate({
+
+                    width: width
+                },
+                    2000,
+                    function () {
+
+                        setImage();
+
+                    });
+            });
+
+            function setImage() {
+
+                $('.current-image').attr('src', urls[steps]).css('width', width);
+
+                if (steps >= urls.length - 1) {
+
+                    steps = 0;
+                }
+                else {
+
+                    steps++;
+                }
+
+                $('.next-image').attr('src', urls[steps]).css('width', 0);
+            }
+        });
+});
