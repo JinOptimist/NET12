18d01b4a7b087515af7f879a658229393b4a94ce
Николай Толкачев <darkmed@mail.ru>
2022-01-14 00:38:22 +0300

    Task251

---
+++ Net12/WebMaze/wwwroot/js/reflectionSpoiler.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $(document).on('click', '.spoiler-trigger', function (e)
+    {
+        e.preventDefault();
+        $(this)
+            .toggleClass('active');
+        $(this)
+            .parent().
+            find('.spoiler-block')
+            .first()
+            .slideToggle(300);
+    })
+});
