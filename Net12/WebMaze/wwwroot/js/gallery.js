8a027fe676275850939193c8227522678c195397
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-13 13:55:09 +0300

    right button without animation

--- Net12/WebMaze/wwwroot/js/gallery.js
+++ Net12/WebMaze/wwwroot/js/gallery.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    let animationTime = 2 * 1000;
+    /*let animationTime = 2 * 1000;
     $('.image').click(function () {
         let image = $(this);
         image.animate(
@@ -12,7 +12,28 @@
             }
         );
 
+    });*/
+
+    let i = 0;
+    let count = $('.pic-item').length;
+
+    $('.pic-item:not([index="' + i + '"])').hide();
+
+    $('.right-button').click(function () {
+        console.log(i);
+        $('[index="' + i + '"]').hide();
+        i = (i + 1) % count;
+        $('[index="' + i + '"]').show();
+        console.log(i);
     });
+
+   /* $('.left-button').click(function () {
+        console.log(i);
+        $('[index="' + i + '"]').hide();
+        i = Math.abs((i - 1) % count);
+        $('[index="' + i + '"]').show();
+        console.log(i);
+    });*/
 
 });
 
