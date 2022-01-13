af565383b90dd92b6a8e2c922ce5e9d255662bea
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-13 21:08:38 +0300

    buttons left and right without animation

--- Net12/WebMaze/wwwroot/js/gallery.js
+++ Net12/WebMaze/wwwroot/js/gallery.js
@@ -1,4 +1,61 @@
 ﻿$(document).ready(function () {
+
+    let i = 0;
+    let j = 0;
+    let count = $('.pic-item').length;
+
+    $('.pic-item:not([index="' + i + '"])').hide();
+
+    $('.right-button').click(function () {     
+        console.log(i);
+        $('[index="' + i + '"]').hide();
+        i = (i + 1) % count;
+        $('[index="' + i + '"]').show();
+        console.log(i);
+    });
+
+    $('.left-button').click(function () {
+        console.log(count);
+        console.log(i);
+        $('[index="' + i + '"]').hide();
+        if (i == 0) {
+            i = count;
+        }
+        else {
+            i -= 1;
+        }
+        $('[index="' + i + '"]').show();
+        console.log(i);
+    });
+
+    /*$('.right-button').click(function () {
+        console.log(i);
+        j = (i + 1) % count;
+
+        function () {
+            $('[index="' + i + '"]').animate({
+
+            });
+        }
+
+        *//*$('[index="' + i + '"]').animate();*//*
+       *//* $('[index="' + i + '"]').show();*//*
+
+        i = j;
+        console.log(i);
+    });*/
+    
+
+    /*$(function () {
+        $("#first").animate({
+            width: '200px'
+        }, { duration: 200, queue: false });
+
+        $("#second").animate({
+            width: '600px'
+        }, { duration: 200, queue: false });
+    });*/
+
     /*let animationTime = 2 * 1000;
     $('.image').click(function () {
         let image = $(this);
@@ -13,28 +70,6 @@
         );
 
     });*/
-
-    let i = 0;
-    let count = $('.pic-item').length;
-
-    $('.pic-item:not([index="' + i + '"])').hide();
-
-    $('.right-button').click(function () {
-        console.log(i);
-        $('[index="' + i + '"]').hide();
-        i = (i + 1) % count;
-        $('[index="' + i + '"]').show();
-        console.log(i);
-    });
-
-   /* $('.left-button').click(function () {
-        console.log(i);
-        $('[index="' + i + '"]').hide();
-        i = Math.abs((i - 1) % count);
-        $('[index="' + i + '"]').show();
-        console.log(i);
-    });*/
-
 });
 
 
