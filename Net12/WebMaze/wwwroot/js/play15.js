b04c2a3c3e7adf53053eaee98f2c17993c2901fc
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-14 22:25:29 +0300

    number were given random place

---
+++ Net12/WebMaze/wwwroot/js/play15.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+    let places = new int[4][4];
+    //[row][column]
+    init();
+    //числа получают места
+    function init () {
+        for (var i = 1; i < 16; i++) {
+            let row, column;
+            do {
+                let randomPlace = getRandomInt(16);
+                row = Math.floor(randomPlace / 4);
+                column = randomPlace % 4;
+            } while (places[row][column] !== undefined);
+
+            places[row][column] = i;
+        }
+
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; i < 4; j++) {
+
+            }
+        }
+
+    }
+
+    
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+});
+
