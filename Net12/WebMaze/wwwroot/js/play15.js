e83c2e3c8d896927cf9b4268f697c3a65714c0c9
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-21 18:48:08 +0300

    pause

--- Net12/WebMaze/wwwroot/js/play15.js
+++ Net12/WebMaze/wwwroot/js/play15.js
@@ -1,6 +1,10 @@
 ﻿$(document).ready(function () {
 
-    let places = new int[4][4];
+    var places = [];
+    places.push(new Array(4));
+    places.push(new Array(4));
+    places.push(new Array(4));
+    places.push(new Array(4));
     //[row][column]
     init();
     //числа получают места
@@ -8,22 +12,27 @@
         for (var i = 1; i < 16; i++) {
             let row, column;
             do {
-                let randomPlace = getRandomInt(16);
+                let randomPlace = getRandomInt(0, 16);
                 row = Math.floor(randomPlace / 4);
                 column = randomPlace % 4;
-            } while (places[row][column] !== undefined);
+            } while (places[row][column] !== undefined);            
 
-            places[row][column] = i;
+            places[row][column] = i;                
         }
 
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; i < 4; j++) {
-
+        for (var i = 0; i < 4; i++) {        
+            for (var j = 0; j < 4; j++) {
+                
+                var element = document.querySelector('[id-row="' + i + '"]', '[id-column="' + j + '"]');
+                console.log(element);
+                var my_html = '<div class="number"><span class="number-style">' + places[i][j] + '</span></div>';
+                element.innerHTML = my_html;
+                
             }
         }
 
     }
-
+    
     
 
     function getRandomInt(min, max) {
