1197d53550be773ed5f3f442e8d1a3db9d0a8934
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-01-23 20:04:08 +0300

    step(up) and check win function were built

--- Net12/WebMaze/wwwroot/js/play15.js
+++ Net12/WebMaze/wwwroot/js/play15.js
@@ -6,38 +6,82 @@
     places.push(new Array(4));
     places.push(new Array(4));
     //[row][column]
+    //place 0..15
+    //number 1..16
+
     init();
-    //числа получают места
+    checkWin();
+    
+
     function init () {
-        for (var i = 1; i < 16; i++) {
+        for (var number = 1; number < 16; number++) {
             let row, column;
             do {
                 let randomPlace = getRandomInt(0, 16);
-                row = Math.floor(randomPlace / 4);
-                column = randomPlace % 4;
-            } while (places[row][column] !== undefined);            
+                cell = getRowAndColumnByPlace(randomPlace);
+            } while (places[cell.row][cell.column] !== undefined);
 
-            places[row][column] = i;                
+            places[cell.row][cell.column] = number;
         }
 
-        for (var i = 0; i < 4; i++) {        
-            for (var j = 0; j < 4; j++) {
-                if (places[i][j]) {
-                    var element = document.querySelector('[id-row="' + i + '"][id-column="' + j + '"]');
-                    console.log(element);
-                    var my_html = '<div class="number"><span class="number-style">' + places[i][j] + '</span></div>';
-                    element.innerHTML = my_html;
-                } else {
-                    var element = document.querySelector('[id-row="' + i + '"][id-column="' + j + '"]');
-                    var my_html = '<div class="number"><span class="number-style">' + '&nbsp;' + '</span></div>';      
-                    element.innerHTML = my_html;
-                }
+        for (let row = 0; row < 4; row++) {        
+            for (let column = 0; column < 4; column++) {
+                if (places[row][column]) {
+                    var element = $('[id-row="' + row + '"][id-column="' + column + '"]');
+                    var piece = $('<div class="number"><span class="number-style">' + places[row][column] + '</span></div>');
+                    piece.click(function () {
+                        step(row, column);
+                        });
+                    element.html(piece);
+                } 
             }
         }
 
     }
-    
-    
+
+    function step(row, column) {
+        /*alert('row ' + row + ', column ' + column);*/
+      /*  $('[id-row="' + row + '"][id-column="' + column + '"] > div')*/
+
+        if (places[row - 1][column] === undefined) {
+            let temp = places[row][column];
+            places[row][column] = undefined;
+            places[row - 1][column] = temp;
+            
+            $('[id-row="' + row + '"][id-column="' + column + '"] > div').appendTo('[id-row="' + (row - 1) + '"][id-column="' + column + '"]');
+            console.log('up');            
+        }
+
+            /*|| places[cell.row + 1][cell.column] == undefined
+            || places[cell.row][cell.column - 1] == undefined || places[cell.row][cell.column + 1] == undefined)*/
+    }
+
+    function checkWin() {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 4; j++) {
+                number = places[i][j];                
+                place = number - 1;
+                cell = getRowAndColumnByPlace(place);                
+
+                if (places[i][j] != places[cell.row][cell.column]) {
+                    console.log(false);
+                    return false;
+                }
+
+            }
+        }
+        console.log('win');
+        return true;
+    }
+
+    function getRowAndColumnByPlace(place) {
+        row = Math.floor(place / 4);
+        column = place % 4;
+        return {
+            row: row,
+            column: column
+        };
+    }
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
