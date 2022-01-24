bf9412f8a036ee4999fa907b6c5a47a0f703b4bc
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2022-01-24 15:11:12 +0300

    001

---
+++ Net12/WebMaze/wwwroot/js/cellInfoHelper.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $('.spoiler-body').css({ 'display': 'none' });
+    $('.botton').click(function () {
+        $(this).next('.spoiler-body').slideToggle(500);
+        return false;
+    });
+
+
+});
+
+
