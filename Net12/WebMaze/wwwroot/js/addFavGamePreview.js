2e79ab3c79f7337e3502be1a9fc785247704f62f
yuulianaa <starrry.sky@yandex.ru>
2022-01-24 20:43:16 +0300

    working on it

--- Net12/WebMaze/wwwroot/js/addFavGamePreview.js
+++ Net12/WebMaze/wwwroot/js/addFavGamePreview.js
@@ -2,7 +2,27 @@
 
     $('textarea[name="Name"]').bind('input', function () {
         let str = $(this).val();
-        $("#i").text(str);
+        $(".favGame-preview-title").text(str);
+    });
+
+    $('textarea[name="Genre"]').bind('input', function () {
+        let str = $(this).val();
+        $(".favGame-preview-genre").text(str);
+    });
+
+    $('textarea[name="YearOfProd"]').bind('input', function () {
+        let str = $(this).val();
+        $(".favGame-preview-year").text(str);
+    });
+
+    $('textarea[name="Desc"]').bind('input', function () {
+        let str = $(this).val();
+        $(".favGame-preview-description").text(str);
+    });
+
+    $('input[name="Rating"]').bind('input', function () {
+        let str = $(this).val();
+        $(".favGame-preview-rating").text(str);
     });
 
 });
