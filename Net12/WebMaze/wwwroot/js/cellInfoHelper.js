9b052a76176743566835ee0dbd11c996c1e19e0d
Metalzonemax <metalzonemax@gmail.com>
2022-01-24 21:31:55 +0300

    006

--- Net12/WebMaze/wwwroot/js/cellInfoHelper.js
+++ Net12/WebMaze/wwwroot/js/cellInfoHelper.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
 
-    $('.spoiler-body').css({ 'display': 'none' });
+    $('.spoiler-body').css('display', 'none');
     $('.spoiler-botton').click(function (event) {
         event.preventDefault();
         $(this).next('.spoiler-body').slideToggle(500);
