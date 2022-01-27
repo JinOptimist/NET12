e25a145c4e0d185d43ad981e57436e83d5aa5425
JinOptimist <88jin@mail.ru>
2022-01-27 21:19:49 +0300

    Fix it

--- Net12/WebMaze/wwwroot/js/miner.js
+++ Net12/WebMaze/wwwroot/js/miner.js
@@ -1,9 +1,9 @@
-﻿"use strict";
-
-/*window.addEventListener('contextmenu', function (e) {
+﻿/*window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 }, false);*/
-   
+
+let cells = [];
+let stillDown = false;
 
 function setFlag(cellId) {
     let data = {
@@ -15,148 +15,72 @@ function setFlag(cellId) {
         });
 };
 
-/*function openNearWithFlags(cellId) {
+//******************************************
+function openNearWithFlagsOrPressNear(cellId) {
+    console.log('main click 01');
     let data = {
         idCell: cellId
     };
-    $.post('/Miner/OpenNearWithFlags', data)
-        .done(function () {
-            location.reload();
-        });
-};*/
+    $.get('/Miner/CheckFlagsAndNearBombsCount', data, function (answer) {
+        console.log('bomb count 02');
+        if (answer) {
+            $.post('/Miner/OpenNearWithFlags', data)
+                .done(function () {
+                    location.reload();
+                });
+        } else {
+            $.get('/Miner/GetNearToPress', data, function (cellsFromServer) {
+                console.log('near data 03');
+                
+                cells = cellsFromServer;
 
+                if (stillDown) {
+                    updateImagesToEmpty(cellsFromServer);
+                }
 
+            }).fail(function () { alert("error1"); });
+        }
 
-
-
-//******************************************
-    function openNearWithFlagsOrPressNear(cellId) {
-        let data = {
-            idCell: cellId
-        };
-        $.get('/Miner/CheckFlagsAndNearBombsCount', data, function (answer) {
-            if (answer) {
-                $.post('/Miner/OpenNearWithFlags', data)
-                    .done(function () {
-                        location.reload();
-                    });
-            }
-            else {
--
-                $.get('/Miner/GetNearToPress', data, function (cells) {
-                        
-                    $(".press").mousedown(function () {
-                        $(".face.miner-elements").attr('src', '../../../images/miner_buttons/face1.png');
-                        for (let i = 0; i < cells.length; i++) {
-                            $("." + cells[i]).attr('src', '../../../images/miner_buttons/t0.png');
-                        }
-
-                    })
-                        .mouseup(function () {
-                            $(".face.miner-elements").attr('src', '../../../images/miner_buttons/face0.png');
-                            for (let i = 0; i < cells.length; i++) {
-                                $("." + cells[i]).attr('src', "../../../images/miner_buttons/miner_button.png");
-                            }
-                            //location.reload();
-                        });
-                    
-
-                }).fail(function () { alert("error1"); });
-            }
-
-        }).fail(function () { alert("error2"); });
+    }).fail(function () { alert("error2"); });
 
 
 
 };
 
+$(document).ready(function () {
+    $(".press")
+        .mousedown(function () {
+            console.log('mousedown 04');
+            $(".face.miner-elements")
+                .attr('src', '/images/miner_buttons/face1.png');
+
+            stillDown = true;
+        })
+        .mouseup(function () {
+            console.log('mouseup 04');
+            $(".face.miner-elements")
+                .attr('src', '../../../images/miner_buttons/face0.png');
+            for (let i = 0; i < cells.length; i++) {
+                $("." + cells[i]).attr('src', "../../../images/miner_buttons/miner_button.png");
+            }
+
+            stillDown = false;
+            //location.reload();
+        });
+});
+
+function updateImagesToEmpty(cells) {
+    for (let i = 0; i < cells.length; i++) {
+        $("." + cells[i]).attr('src', '../../../images/miner_buttons/t0.png');
+    }
+}
+
+function updateImagesToCommon(cells) {
+    for (let i = 0; i < cells.length; i++) {
+        $("." + cells[i]).attr('src', "../../../images/miner_buttons/miner_button.png");
+    }
+}
+
 //******************************************
 
 
-
-/*
-$('#Clicker').mousedown(function () {
-    //do something here
-    timeout = setInterval(function () {
-        //do same thing here again
-    }, 500);
-
-    return false;
-});
-$('#Clicker').mouseup(function () {
-    clearInterval(timeout);
-    return false;
-});
-$('#Clicker').mouseout(function () {
-    clearInterval(timeout);
-    return false;
-});*/
-
-/*timer();
-
-function timer() {
-    $.get('/Miner/SetUpTimerForStartedGame')
-        .done(function (fieldId) {
-            if (!localStorage.getItem(fieldId)) {
-
-            }
-        })
-    };*/
-
-
-
-/*let i = 0;
-let k = 0;
-let h = 0;
-
-let timer = 0;
-
-
-let cl1 = new Image(); cl1.src = '../../../images/miner_buttons/d1.png';
-let cl2 = new Image(); cl2.src = '../../../images/miner_buttons/d2.png';
-let cl3 = new Image(); cl3.src = '../../../images/miner_buttons/d3.png';
-let cl4 = new Image(); cl4.src = '../../../images/miner_buttons/d4.png';
-let cl5 = new Image(); cl5.src = '../../../images/miner_buttons/d5.png';
-let cl6 = new Image(); cl6.src = '../../../images/miner_buttons/d6.png';
-let cl7 = new Image(); cl7.src = '../../../images/miner_buttons/d7.png';
-let cl8 = new Image(); cl8.src = '../../../images/miner_buttons/d8.png';
-let cl9 = new Image(); cl9.src = '../../../images/miner_buttons/d9.png';
-let cl0 = new Image(); cl0.src = '../../../images/miner_buttons/d0.png';
-let clock_numbers = new Array(cl0, cl1, cl2, cl3, cl4, cl5, cl6, cl7, cl8, cl9);
-*/
-
-
-/*
-
-
-function time1()
-{
-    if (i >= 10) {
-        i = 0;
-    }
-    if (h = 10) {
-        return false;
-    }
-    document.getElementById('imag1').src = clock_numbers[i].src;
-    i += 1;
-    setTimeout(time1, 1000);
-}
-function time2() {
-    if (k >= 10) {
-        k = 0;
-    }
-    if (h = 10) {
-        return false;
-    }
-    document.getElementById('imag2').src = clock_numbers[k].src;
-    k += 1;
-    setTimeout(time2, 10000);
-}
-function time3() {
-    if (h = 10) {
-        return false;
-    }
-    document.getElementById('imag3').src = clock_numbers[h].src;
-    h += 1;
-    setTimeout(time3, 100000);
-}*/
