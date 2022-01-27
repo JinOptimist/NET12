b7498682baab8f5e0ab334de43247b3e59698cd6
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2022-01-27 17:15:45 +0300

    003

--- Net12/WebMaze/Models/PermissionViewModel.cs
+++ Net12/WebMaze/Models/PermissionViewModel.cs
@@ -7,11 +7,8 @@ namespace WebMaze.Models
 {
     public class PermissionViewModel
     {
-
         public string Name { get; set; }
         public string Desc { get; set; }
-
         public int Count { get; set; }
-
     }
 }
