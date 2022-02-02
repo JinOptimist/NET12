23b5c46ea533c498f9a080218262ed1f594fa5e4
Kadabras <samex45@gmail.com>
2022-02-02 12:30:26 +0300

    fix

--- Net12/WebMaze/wwwroot/js/transaction.js
+++ Net12/WebMaze/wwwroot/js/transaction.js
@@ -6,6 +6,8 @@
         let transactionUserName = $('.transactionUserName').val();
         let transactionCoins = $('.transactionCoins').val();
 
+        $('.transaction-info').text("Обработка запроса...");
+
         $.post('/User/JSTransactionCoins', { userName: transactionUserName, coins: transactionCoins})
             .done(function (answer) {
 
