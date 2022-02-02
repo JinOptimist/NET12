76b466882a8638eccfec9cfd724a983c66670d67
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2022-02-02 21:25:38 +0300

    start

--- Net12/WebMaze/Models/NewCellSuggestionViewModel.cs
+++ Net12/WebMaze/Models/NewCellSuggestionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -18,5 +19,6 @@ namespace WebMaze.Models
         public int FatigueChange { get; set; }
         public string UserName { get; set; }
         public int GlobalUserRating { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
