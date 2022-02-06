bcc210319f7eb691114610b922e0b2bf7364d85d
Kadabras <samex45@gmail.com>
2022-02-06 16:28:14 +0300

    fix

--- Net12/WebMaze/Controllers/UserController.cs
+++ Net12/WebMaze/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
