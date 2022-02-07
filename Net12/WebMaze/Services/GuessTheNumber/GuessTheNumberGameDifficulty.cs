e61566544e1c9446c92b13e98a76b484b3a8a00b
Николай Толкачев <darkmed@mail.ru>
2022-02-07 18:31:13 +0300

    Task289

--- Net12/WebMaze/Services/GuessTheNumber/GuessTheNumberGameDifficulty.cs
+++ Net12/WebMaze/Services/GuessTheNumber/GuessTheNumberGameDifficulty.cs
@@ -15,33 +15,5 @@ namespace WebMaze.Services.GuessTheNumber
         Medium = 2,
         [Display(Name ="Hard", ResourceType = typeof(WebMaze.ResourceLocalization.GuessTheNumber))]
         Hard = 3
-    }
-    //public static class EnumExtensions
-    //{
-    //    public static string GetDisplayName(this GuessTheNumberGameDifficulty enumValue)
-    //    {
-    //        var fi = enumValue.GetType().GetField(enumValue.ToString());
-
-    //        var attributes = (DisplayAttribute[])fi.GetCustomAttributes(typeof(DisplayAttribute), false);
-
-    //        return attributes != null && attributes.Length > 0
-    //                ? attributes[0].GetName()
-    //                : enumValue.ToString();
-    //    }
-    //}
-
-    //public static class DifficultyExt
-    //{
-    //    public static string AsDisplayString(this GuessTheNumberGameDifficulty difficulty)
-    //    {
-    //        switch (difficulty)
-    //        {
-    //            case GuessTheNumberGameDifficulty.Easy: return ResourceLocalization.GuessTheNumber.Easy;
-    //            case GuessTheNumberGameDifficulty.Medium: return ResourceLocalization.GuessTheNumber.Medium;
-    //            case GuessTheNumberGameDifficulty.Hard: return ResourceLocalization.GuessTheNumber.Hard;
-
-    //            default: throw new ArgumentOutOfRangeException("difficulty");
-    //        }
-    //    }
-    //}
+    }    
 }
