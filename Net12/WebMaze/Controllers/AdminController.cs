47af3c46f0acc31c4426b7f51af1541fe0b6b779
Metalzonemax <metalzonemax@gmail.com>
2022-02-10 20:30:30 +0300

    001

--- Net12/WebMaze/Controllers/AdminController.cs
+++ Net12/WebMaze/Controllers/AdminController.cs
@@ -28,8 +28,7 @@ namespace WebMaze.Controllers
         private IMapper _mapper;
         private UserRepository _userRepository;
         private CellInfoHelperService _cellInfoHelperService;
-        public AdminController(PermissionRepository permissionRepository, IMapper mapper, UserRepository userRepository)
-        public AdminController(PermissionRepository permissionRepository, IMapper mapper, CellInfoHelperService cellInfoHelperService)
+        public AdminController(PermissionRepository permissionRepository, IMapper mapper, UserRepository userRepository, CellInfoHelperService cellInfoHelperService)
         {
             _permissionRepository = permissionRepository;
             _mapper = mapper;
@@ -37,192 +36,192 @@ namespace WebMaze.Controllers
             _cellInfoHelperService = cellInfoHelperService;
         }
 
-        public IActionResult ViewPermission()
+    public IActionResult ViewPermission()
+    {
+        var permissionViewModels = new List<PermissionViewModel>();
+        permissionViewModels = _permissionRepository.GetAll()
+            .Select(x => _mapper.Map<PermissionViewModel>(x)).ToList();
+        return View(permissionViewModels);
+    }
+
+
+    public IActionResult EditingUsers()
+    {
+        return View();
+    }
+
+    public IActionResult ReflectionPages()
+    {
+        var controllers = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(x => x.BaseType == typeof(Controller));
+
+        var viewModels = new List<ControllerViewModel>();
+
+        foreach (var controller in controllers)
         {
-            var permissionViewModels = new List<PermissionViewModel>();
-            permissionViewModels = _permissionRepository.GetAll()
-                .Select(x => _mapper.Map<PermissionViewModel>(x)).ToList();
-            return View(permissionViewModels);
-        }
+            var viewModel = new ControllerViewModel();
 
+            viewModel.Name = controller.Name.Replace("Controller", "");
 
-        public IActionResult EditingUsers()
-        {
-            return View();
-        }
+            var actions = controller
+                .GetMethods()
+                .Where(x => x.ReturnType == typeof(IActionResult));
 
-        public IActionResult ReflectionPages()
-        {
-            var controllers = Assembly
-                .GetExecutingAssembly()
-                .GetTypes()
-                .Where(x => x.BaseType == typeof(Controller));
-
-            var viewModels = new List<ControllerViewModel>();
-
-            foreach (var controller in controllers)
+            viewModel.Actions = new List<ActionViewModel>();
+            foreach (var action in actions)
             {
-                var viewModel = new ControllerViewModel();
+                var actionViewModel = new ActionViewModel();
 
-                viewModel.Name = controller.Name.Replace("Controller", "");
-
-                var actions = controller
-                    .GetMethods()
-                    .Where(x => x.ReturnType == typeof(IActionResult));
-
-                viewModel.Actions = new List<ActionViewModel>();
-                foreach (var action in actions)
-                {
-                    var actionViewModel = new ActionViewModel();
-
-                    actionViewModel.Name = action.Name;
-                    actionViewModel.AttributeNames = action
-                        .CustomAttributes
-                        .Select(x => x.AttributeType.Name.Replace("Attribute", ""))
-                        .ToList();
-
-                    actionViewModel.ParamsNames = action
-                        .GetParameters()
-                        .Select(x => x.Name)
-                        .ToList();
-
-                    viewModel.Actions.Add(actionViewModel);
-                }
-
-                viewModel.ActionCount = actions.Count();
-
-                viewModel.AttributeNames = controller
+                actionViewModel.Name = action.Name;
+                actionViewModel.AttributeNames = action
                     .CustomAttributes
                     .Select(x => x.AttributeType.Name.Replace("Attribute", ""))
                     .ToList();
 
-                viewModels.Add(viewModel);
+                actionViewModel.ParamsNames = action
+                    .GetParameters()
+                    .Select(x => x.Name)
+                    .ToList();
+
+                viewModel.Actions.Add(actionViewModel);
             }
 
-            return View(viewModels);
+            viewModel.ActionCount = actions.Count();
+
+            viewModel.AttributeNames = controller
+                .CustomAttributes
+                .Select(x => x.AttributeType.Name.Replace("Attribute", ""))
+                .ToList();
+
+            viewModels.Add(viewModel);
         }
-        public IActionResult CellInfoHelper()
+
+        return View(viewModels);
+    }
+    public IActionResult CellInfoHelper()
+    {
+        List<string> namesTypeOfCell = _cellInfoHelperService.GetNamesTypeOfCell();
+
+        return View(namesTypeOfCell);
+    }
+
+
+    public IActionResult DownloadTreeControllers()
+    {
+        using (var ms = new MemoryStream())
         {
-            List<string> namesTypeOfCell = _cellInfoHelperService.GetNamesTypeOfCell();
-
-            return View(namesTypeOfCell);
-        }
-
-
-        public IActionResult DownloadTreeControllers()
-        {
-            using (var ms = new MemoryStream())
+            using (var wordDocument = WordprocessingDocument.Create(ms, WordprocessingDocumentType.Document))
             {
-                using (var wordDocument = WordprocessingDocument.Create(ms, WordprocessingDocumentType.Document))
-                {
-                    var controllers = Assembly
-                        .GetExecutingAssembly()
-                        .GetTypes()
-                        .Where(x => x.BaseType == typeof(Controller));
+                var controllers = Assembly
+                    .GetExecutingAssembly()
+                    .GetTypes()
+                    .Where(x => x.BaseType == typeof(Controller));
 
+                var mainPart = wordDocument.AddMainDocumentPart();
+                mainPart.Document = new Document();
+                var body = mainPart.Document.AppendChild(new Body());
+
+                foreach (var controller in controllers)
+                {
+
+                    var para = body.AppendChild(new Paragraph());
+                    var runTitle = para.AppendChild(new Run());
+                    var prop = runTitle.AppendChild(new RunProperties());
+
+                    prop.AppendChild(new Color { Val = "008080" });
+                    prop.AppendChild(new FontSize { Val = "36" });
+
+                    var text = runTitle.AppendChild(new Text());
+                    text.Text = controller.Name + "()";
+
+                    var actions = controller
+                        .GetMethods()
+                        .Where(x => x.ReturnType == typeof(IActionResult));
+
+                    foreach (var action in actions)
+                    {
+                        var paramsString = "";
+
+                        foreach (var param in action.GetParameters())
+                        {
+                            paramsString += param.Name + ", ";
+                        }
+
+                        if (paramsString.Length > 0)
+                        {
+                            paramsString = paramsString.Remove(paramsString.Length - 2);
+                        }
+
+                        var runTitleAction = para.AppendChild(new Run());
+                        runTitleAction.AppendChild(new RunProperties { Color = new Color { Val = "006400" } });
+                        runTitleAction.AppendChild(new Break());
+                        runTitleAction.AppendChild(new TabChar());
+                        runTitleAction.AppendChild(new Text(action.Name.ToString() + "("));
+
+                        var runTitleAction2 = para.AppendChild(new Run());
+                        runTitleAction2.AppendChild(new RunProperties { Color = new Color { Val = "191970" } });
+                        runTitleAction2.AppendChild(new Text(paramsString));
+
+                        var runTitleAction3 = para.AppendChild(new Run());
+                        runTitleAction3.AppendChild(new RunProperties { Color = new Color { Val = "006400" } });
+                        runTitleAction3.AppendChild(new Text(")"));
+                    }
+                }
+
+                wordDocument.Close();
+            }
+
+            return File(ms.ToArray(),
+               "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+               "ReflectionPages.docx");
+        }
+    }
+
+    public IActionResult DownloadCellInfoHelper()
+    {
+        byte[] answer = _cellInfoHelperService.CreateDeveloperChallenge();
+
+        return File(answer,
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                "CellInfoHelper.docx");
+    }
+
+    public IActionResult DownlodListReapitUsers()
+    {
+        using (var ms = new MemoryStream())
+        {
+            using (var wordDocument = WordprocessingDocument.Create(ms, WordprocessingDocumentType.Document))
+            {
+                var listUsers = _userRepository.GetReapitUsersNameSQL();
+                if (listUsers.Any())
+                {
                     var mainPart = wordDocument.AddMainDocumentPart();
                     mainPart.Document = new Document();
                     var body = mainPart.Document.AppendChild(new Body());
 
-                    foreach (var controller in controllers)
+                    foreach (var user in listUsers)
                     {
-
                         var para = body.AppendChild(new Paragraph());
                         var runTitle = para.AppendChild(new Run());
-                        var prop = runTitle.AppendChild(new RunProperties());
-
-                        prop.AppendChild(new Color { Val = "008080" });
-                        prop.AppendChild(new FontSize { Val = "36" });
-
-                        var text = runTitle.AppendChild(new Text());
-                        text.Text = controller.Name + "()";
-
-                        var actions = controller
-                            .GetMethods()
-                            .Where(x => x.ReturnType == typeof(IActionResult));
-
-                        foreach (var action in actions)
-                        {
-                            var paramsString = "";
-
-                            foreach (var param in action.GetParameters())
-                            {
-                                paramsString += param.Name + ", ";
-                            }
-
-                            if (paramsString.Length > 0)
-                            {
-                                paramsString = paramsString.Remove(paramsString.Length - 2);
-                            }
-
-                            var runTitleAction = para.AppendChild(new Run());
-                            runTitleAction.AppendChild(new RunProperties { Color = new Color { Val = "006400" } });
-                            runTitleAction.AppendChild(new Break());
-                            runTitleAction.AppendChild(new TabChar());
-                            runTitleAction.AppendChild(new Text(action.Name.ToString() + "("));
-
-                            var runTitleAction2 = para.AppendChild(new Run());
-                            runTitleAction2.AppendChild(new RunProperties { Color = new Color { Val = "191970" } });
-                            runTitleAction2.AppendChild(new Text(paramsString));
-
-                            var runTitleAction3 = para.AppendChild(new Run());
-                            runTitleAction3.AppendChild(new RunProperties { Color = new Color { Val = "006400" } });
-                            runTitleAction3.AppendChild(new Text(")"));
-                        }
+                        runTitle.AppendChild(new Text($"{user.Name} - {user.Id}"));
+                        var properties = new ParagraphProperties();
+                        var fontSize = new FontSize() { Val = "36" };
+                        properties.Append(fontSize);
+                        para.Append(properties);
                     }
 
                     wordDocument.Close();
+
+                    return File(ms.ToArray(),
+                       "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                       "ReapitUsersName.docx");
+
                 }
-
-                return File(ms.ToArray(),
-                   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                   "ReflectionPages.docx");
-            }
-        }
-
-        public IActionResult DownloadCellInfoHelper()
-        {
-            byte[] answer = _cellInfoHelperService.CreateDeveloperChallenge();
-
-            return File(answer,
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                    "CellInfoHelper.docx");
-        }
-
-        public IActionResult DownlodListReapitUsers()
-        {
-            using (var ms = new MemoryStream())
-            {
-                using (var wordDocument = WordprocessingDocument.Create(ms, WordprocessingDocumentType.Document))
-                {
-                    var listUsers = _userRepository.GetReapitUsersNameSQL();
-                    if (listUsers.Any())
-                    {
-                        var mainPart = wordDocument.AddMainDocumentPart();
-                        mainPart.Document = new Document();
-                        var body = mainPart.Document.AppendChild(new Body());
-
-                        foreach (var user in listUsers)
-                        {
-                            var para = body.AppendChild(new Paragraph());
-                            var runTitle = para.AppendChild(new Run());
-                            runTitle.AppendChild(new Text($"{user.Name} - {user.Id}"));
-                            var properties = new ParagraphProperties();
-                            var fontSize = new FontSize() { Val = "36" };
-                            properties.Append(fontSize);
-                            para.Append(properties);
-                        }
-
-                        wordDocument.Close();
-
-                        return File(ms.ToArray(),
-                           "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                           "ReapitUsersName.docx");
-
-                    }
-                    return RedirectToAction("Profile", "User");
-                }
+                return RedirectToAction("Profile", "User");
             }
         }
     }
+}
 }
