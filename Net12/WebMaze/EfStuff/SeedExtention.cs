e83ede4fd356051dce1d4f007277fd97ae381e77
O2Mask <O2Mask@DESKTOP-SVN1RNS>
2022-02-16 21:27:12 +0300

    Seed work

--- Net12/WebMaze/EfStuff/SeedExtention.cs
+++ Net12/WebMaze/EfStuff/SeedExtention.cs
@@ -29,6 +29,7 @@ namespace WebMaze.EfStuff
                 SeedGallery(scope);
                 SeedZumaGameDifficult(scope);
                 SeedGuessTheNumberGameParametersRecords(scope);
+                SeedNewCellSugg(scope);
             }
 
             return host;
@@ -239,6 +240,49 @@ namespace WebMaze.EfStuff
                 };
                 seedGameParametrs.Save(guessTheNumberGameParameterHard);
             }
+        }
+
+        private static void SeedNewCellSugg(IServiceScope scope)
+        {
+            var newCellSuggRepository = scope.ServiceProvider.GetService<NewCellSuggRepository>();
+            var userRepository = scope.ServiceProvider.GetService<UserRepository>();
+            int countTestEntry = 5; // Here you can set the required number of test records in the database NewCellSuggestions.
+
+            if (newCellSuggRepository.Count() < countTestEntry)
+            {
+                var namesTestUsers = new List<string>() { "Bob", "Sam", "Tom", "Mike" };
+
+                for (int i = 0; i < namesTestUsers.Count; i++)
+                {
+                    var testUser = new User()
+                    {
+                        Name = namesTestUsers[i],
+                        Password = "1234",
+                        Coins = 100,
+                        Age = 18,
+                        IsActive = true,
+                        GlobalUserRating = 100
+                    };
+
+                    userRepository.Save(testUser);
+
+                }
+
+                for (int i = 0; i < countTestEntry; i++)
+                {
+                    var testNewCellSugg = new NewCellSuggestion()
+                    {
+                        Title = $"TestCellSugg-{i}",
+                        IsActive = true,
+                        Url = "/imgYellowTeam/stoc.jpg",
+                        Creater = userRepository.GetUserByName(namesTestUsers[new Random().Next(namesTestUsers.Count)])
+                    };
+
+                    newCellSuggRepository.Save(testNewCellSugg);
+                }
+
+            }
+
         }
     }
 }
