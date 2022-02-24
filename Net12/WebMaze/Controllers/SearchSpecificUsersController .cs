139600141acf8f7630e6357472f6430c786d898b
Metalzonemax <metalzonemax@gmail.com>
2022-02-24 21:04:04 +0300

    Base

---
+++ Net12/WebMaze/Controllers/SearchSpecificUsersController .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.Controllers
+{
+    public class SearchSpecificUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CellSuggestionWinner()
+        {
+            var myList = new List<string>() {"Bibi", "Tobi" };
+            return View(myList);
+        }
+    }
+}
