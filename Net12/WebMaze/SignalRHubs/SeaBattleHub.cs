78bea1a28e481a1568a62c41c524d18c17903c61
andreyuqaaaa <danger_box@inbox.ru>
2022-03-03 21:29:25 +0300

    begin

---
+++ Net12/WebMaze/SignalRHubs/SeaBattleHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.SignalRHubs
+{
+    public class SeaBattleHub : Hub
+    {
+
+        public async Task AIShooting()
+        {
+
+            //var name = _userService.GetCurrentUser().Name;
+            await Clients.All.SendAsync("NewMessage", "message");
+
+        }
+    }
+}
---
+++ Net12/WebMaze/wwwroot/js/seaBattle.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    let timer = 10;
+
+    const hubConnection = new signalR.HubConnectionBuilder()
+        .withUrl("/seaBattle")
+        .build();
+
+    //Что делать, когда пршло новое сообщение
+    hubConnection.on("NewMessage", function (message) {
+        location.reload();
+    });
+
+
+    setInterval(function () {
+
+        if (timer < 0) {
+            timer = 10;
+        }
+        $('.seaBattleTimer').text(timer);
+
+
+        if (timer == 0) {
+            //Отправить новое сообщзение на сервер
+            hubConnection.invoke("AIShooting");
+        }
+
+
+        timer--;
+    }, 1000);
+
+    hubConnection.start();
+});
