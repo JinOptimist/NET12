45e5299baf970803ac8195cb0c89ae063509b79d
Metalzonemax <metalzonemax@gmail.com>
2022-03-09 12:48:47 +0300

    002

--- Net12/WebMaze/Controllers/SearchSpecificUsersController .cs
+++ Net12/WebMaze/Controllers/SearchSpecificUsersController .cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebMaze.EfStuff.DbModel;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using WebMaze.EfStuff.Repositories;
 
 namespace WebMaze.Controllers
 {
     public class SearchSpecificUsersController : Controller
     {
+        private SearchSpecificUsersRepository _searchSpecificUsersRepository;
+        public SearchSpecificUsersController(SearchSpecificUsersRepository searchSpecificUsersRepository)
+        {
+            _searchSpecificUsersRepository = searchSpecificUsersRepository;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,8 +24,7 @@ namespace WebMaze.Controllers
 
         public IActionResult CellSuggestionWinner()
         {
-            var myList = new List<User>();
-            myList
+            var myList = _searchSpecificUsersRepository.GetWonUsersInNewCellSugg();
 
             return View(myList);
         }
---
+++ Net12/WebMaze/EfStuff/Repositories/SearchSpecificUsersRepository.cs
@@ -0,0 +1,30 @@
+﻿using WebMaze.EfStuff.DbModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+
+namespace WebMaze.EfStuff.Repositories
+{
+    public class SearchSpecificUsersRepository : BaseRepository<User>
+    {
+        public SearchSpecificUsersRepository(WebContext webContext) : base(webContext)
+        {
+        }
+
+        public List<User> GetWonUsersInNewCellSugg()
+        {
+            return _dbSet.FromSqlRaw<User>($@"
+ SELECT * FROM Users WHERE Id in
+(SELECT CreaterId FROM  (SELECT CreaterId, COUNT(*) as amount FROM (SELECT CreaterId FROM
+(SELECT CreaterId, Users.IsActive FROM NewCellSuggestions as N INNER JOIN Users ON N.CreaterId = Users.Id) as temp WHERE IsActive = 1) as temp GROUP BY CreaterId) as temp
+WHERE amount = (SELECT max(amount) from (SELECT CreaterId, COUNT(*) as amount FROM (SELECT CreaterId FROM
+(SELECT CreaterId, Users.IsActive FROM NewCellSuggestions as N INNER JOIN Users ON N.CreaterId = Users.Id) as temp WHERE IsActive = 1) as temp GROUP BY CreaterId) as temp))  
+    ")
+                .ToList();
+        }
+    }
+}
+
