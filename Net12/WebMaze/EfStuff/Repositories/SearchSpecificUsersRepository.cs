6683b3eddea2155dae9791ebfcbf4090e81da357
Metalzonemax <metalzonemax@gmail.com>
2022-03-10 19:33:36 +0300

    FINAL

--- Net12/WebMaze/EfStuff/Repositories/SearchSpecificUsersRepository.cs
+++ Net12/WebMaze/EfStuff/Repositories/SearchSpecificUsersRepository.cs
@@ -14,12 +14,36 @@ namespace WebMaze.EfStuff.Repositories
         public List<User> GetWonUsersInNewCellSugg()
         {
             return _dbSet.FromSqlRaw<User>($@"
- SELECT * FROM Users WHERE Id in
-(SELECT CreaterId FROM  (SELECT CreaterId, COUNT(*) as amount FROM (SELECT CreaterId FROM
-(SELECT CreaterId, Users.IsActive FROM NewCellSuggestions as N INNER JOIN Users ON N.CreaterId = Users.Id) as temp WHERE IsActive = 1) as temp GROUP BY CreaterId) as temp
-WHERE amount = (SELECT max(amount) from (SELECT CreaterId, COUNT(*) as amount FROM (SELECT CreaterId FROM
-(SELECT CreaterId, Users.IsActive FROM NewCellSuggestions as N INNER JOIN Users ON N.CreaterId = Users.Id) as temp WHERE IsActive = 1) as temp GROUP BY CreaterId) as temp))  
-    ")
+ SELECT *
+FROM Users
+WHERE Id in
+    (SELECT CreaterId
+     FROM
+       (SELECT CreaterId,
+               COUNT(*) AS amount
+        FROM
+          (SELECT CreaterId
+           FROM
+             (SELECT CreaterId,
+                     Users.IsActive
+              FROM NewCellSuggestions AS N
+              INNER JOIN Users ON N.CreaterId = Users.Id) AS TEMP
+           WHERE IsActive = 1) AS TEMP
+        GROUP BY CreaterId) AS TEMP
+     WHERE amount =
+         (SELECT max(amount)
+          FROM
+            (SELECT CreaterId,
+                    COUNT(*) AS amount
+             FROM
+               (SELECT CreaterId
+                FROM
+                  (SELECT CreaterId,
+                          Users.IsActive
+                   FROM NewCellSuggestions AS N
+                   INNER JOIN Users ON N.CreaterId = Users.Id) AS TEMP
+                WHERE IsActive = 1) AS TEMP
+             GROUP BY CreaterId) AS TEMP))")
                 .ToList();
         }
     }
