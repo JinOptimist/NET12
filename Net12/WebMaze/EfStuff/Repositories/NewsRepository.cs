f6ba0b0b2b2b7d54dfe0e82d37360fec04bc8f12
Vladislau Zhuk <86854997+SnikersFar@users.noreply.github.com>
2022-03-11 11:18:09 +0300

    add searcher

--- Net12/WebMaze/EfStuff/Repositories/NewsRepository.cs
+++ Net12/WebMaze/EfStuff/Repositories/NewsRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using WebMaze.EfStuff.DbModel;
 
@@ -32,12 +33,61 @@ namespace WebMaze.EfStuff.Repositories
         {
             return _dbSet.SingleOrDefault(x => x.Title == title);
         }
-        
+
 
         public List<News> GetForPagination(int perPage, int page, string columnName = "CreationDate")
             => GetSortedNews(columnName)
             .Skip((page - 1) * perPage)
             .Take(perPage)
             .ToList();
+
+
+        public List<News> GetFiltList(int TypeOfSearch, string columnName, string TextSearch)
+        {
+
+
+            List<News> MyList;
+            var table = Expression.Parameter(typeof(News), "news");// news =>
+
+            var ListOfProperty = columnName.Split(".");
+            var member = Expression.Property(table, ListOfProperty[0]);
+            for (int i = 1; i < ListOfProperty.Length; i++)
+            {
+                var item = ListOfProperty[i];
+                var next = Expression.Property(member, item);
+                member = next;
+
+            }
+
+            var constName = Expression.Constant(TextSearch); // 'good news'
+
+            if (TypeOfSearch == 1)
+            {
+     
+
+                var eq = Expression.Equal(Expression.Convert(Expression.Convert(member,typeof(object)),typeof(string)), constName);// news => news.Title == 'good news'
+                var condition = Expression.Lambda<Func<News, bool>>(eq, table);
+
+                MyList = _dbSet.Where(condition).ToList();
+            } else if(TypeOfSearch == 2)
+            {
+
+                
+                MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+                var containsMethodExp = Expression.Call(Expression.Convert(Expression.Convert(member, typeof(object)), typeof(string)), method, constName);
+
+                
+                var condition = Expression.Lambda<Func<News, bool>>(Expression.Convert(containsMethodExp, typeof(bool)), table);
+
+                MyList = _dbSet.Where(condition).ToList();
+            } else
+            {
+                MyList = new List<News>();
+            }
+
+            return MyList;
+        }
     }
+
+
 }
