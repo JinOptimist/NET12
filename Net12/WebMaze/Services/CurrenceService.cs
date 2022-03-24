0e6f940630c55e41c379750a80c040ea9a8a2963
Aleksei <TheEclipsE@tut.by>
2022-03-24 21:25:49 +0300

    edit

--- Net12/WebMaze/Services/CurrenceService.cs
+++ Net12/WebMaze/Services/CurrenceService.cs
@@ -9,14 +9,13 @@ using WebMaze.Models.CurrencyDto;
 
 namespace WebMaze.Services
 {
-    public class CurrenceService
+    public class CurrenceService 
     {
         public async Task<List<CurrencyRateDto>> GetRates()
         {
             var client = new HttpClient();
             var dateStr = DateTime.Now.ToString("yyyy-M-d");//2016-7-6
-            var uri = new Uri($"https://www.nbrb.by/api/exrates/rates?ondate={dateStr}&periodicity=0");
-            
+            var uri = new Uri($"https://www.nbrb.by/api/exrates/rates?ondate={dateStr}&periodicity=0");            
             
             var response = await client.GetAsync(uri);
 
@@ -29,14 +28,9 @@ namespace WebMaze.Services
 
         public async Task<List<CurrencyRate>> GetAllCurrencies()
         {
-            var client = new HttpClient();
             var uri = new Uri("https://www.nbrb.by/api/exrates/rates?periodicity=0");
 
-            var response = await client.GetAsync(uri);
-
-            var json = await response.Content.ReadAsStringAsync();
-
-            var currencies = JsonConvert.DeserializeObject<List<CurrencyRate>>(json);
+            var currencies = await CallAPI<List<CurrencyRate>>(uri);
 
             return currencies;
         }
@@ -45,14 +39,12 @@ namespace WebMaze.Services
         {
             var uri = new Uri($"https://www.nbrb.by/api/exrates/rates/{cur_Id}");
 
-            var json = await CallAPI(uri);
-
-            var rate = JsonConvert.DeserializeObject<CurrencyRate>(json);
+            var rate = await CallAPI<CurrencyRate>(uri);
 
             return rate;
         }
 
-        private async Task<string> CallAPI(Uri uri)
+        private async Task<Template> CallAPI<Template>(Uri uri)
         {
             var client = new HttpClient();
 
@@ -60,7 +52,9 @@ namespace WebMaze.Services
 
             var json = await response.Content.ReadAsStringAsync();
 
-            return json;
+            var result = JsonConvert.DeserializeObject<Template>(json);
+
+            return result;
         }
     }
 }
