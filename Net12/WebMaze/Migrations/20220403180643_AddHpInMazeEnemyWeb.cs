4e69db8d881b9ed1eb94d97cce5c03a000cb81e0
Metalzonemax <metalzonemax@gmail.com>
2022-04-03 22:05:28 +0300

    HpEnemy

---
+++ Net12/WebMaze/Migrations/20220403180643_AddHpInMazeEnemyWeb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMaze.Migrations
+{
+    public partial class AddHpInMazeEnemyWeb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Hp",
+                table: "MazeEnemyWeb",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hp",
+                table: "MazeEnemyWeb");
+        }
+    }
+}
