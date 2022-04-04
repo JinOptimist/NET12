2388812758f97442dc89ba6c800ba4666efdb42f
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-04-04 20:51:43 +0300

    buttons like/dislike can be pressed

--- Net12/WebMaze/wwwroot/js/galleryAjax.js
+++ Net12/WebMaze/wwwroot/js/galleryAjax.js
@@ -3,10 +3,19 @@
         if ($(this).attr('data-id') != "sortButton") {
             evt.preventDefault();
 
+            if ($(this).hasClass("change-color-good")) {
+                $(this).toggleClass("change-color-good");
+            }
+            else {
+                $(this).addClass("change-color-good");
+                $('.bad-button').removeClass("change-color-bad");
+            }
+
             $.ajax({
                 url: '/Gallery/Wonderful',
                 data: { imageId: $(this).attr('data-id') }
             })
+            
         }
         
     });
@@ -14,9 +23,18 @@
     $('.bad-button').click(function (evt) {
         evt.preventDefault();
 
+        if ($(this).hasClass("change-color-bad")) {
+            $(this).toggleClass("change-color-bad");
+        }
+        else {
+            $(this).addClass("change-color-bad");
+            $('.good-button').removeClass("change-color-good");
+        }
+
         $.ajax({
             url: '/Gallery/Awful',
             data: { imageId: $(this).attr('data-id') }
         })
+        
     });
 });
